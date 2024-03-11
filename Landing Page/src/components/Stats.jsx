352f201aebc799c8987cfdcf0e781baa35c2371a
shanukafer98 <shanukafer98@gmail.com>
2024-03-12 00:12:42 +0530

    Update duration of CountUp animation in Stats component

--- Landing Page/src/components/Stats.jsx
+++ Landing Page/src/components/Stats.jsx
@@ -9,7 +9,7 @@ const Stats = () => (
       {stats.map((stat) => (
         <div key={stat.id} className={`flex-1 flex justify-center items-center flex-col sm:flex-row`}>
           <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white">
-            <CountUp end={parseFloat(stat.value.replace(/[^0-9.]/g, ""))} duration={2} />
+            <CountUp end={parseFloat(stat.value.replace(/[^0-9.]/g, ""))} duration={7} />
             {stat.value.includes('M') && 'M'}+
           </h4>
           <div>
