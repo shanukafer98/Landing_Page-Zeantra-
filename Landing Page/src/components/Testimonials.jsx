740a9da4be55de8f96ad7e81db09ab6bab2884ee
shanukafer98 <shanukafer98@gmail.com>
2024-03-12 00:11:55 +0530

    Update testimonial heading to be more generic

--- Landing Page/src/components/Testimonials.jsx
+++ Landing Page/src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h2 className={styles.heading2}>
-        What <span className="text-gradient">People are  saying</span> about Zeantra?
+        What <span className="text-gradient">People are  saying</span> about us?
       </h2>
       {/* <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
